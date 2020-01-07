a4683ac4f8ffd82ef5c93c3632cae568663da090
PtahsS <ptahsrg@gmail.com>
2020-01-08 01:16:42 +0200

    state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,3 +10,22 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component{
+	constructor(){
+		super()
+		this.state={
+			answer: "yes"
+		}
+	}
+	render(){
+		return(
+			<h2>Is it so important to know language? Oh, {this.state.answer}</h2>
+			)
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
