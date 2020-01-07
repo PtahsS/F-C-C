bb8f7ec1af533d8fe30dcc09b827f0a83ce06694
PtahsS <ptahsrg@gmail.com>
2020-01-07 16:16:10 +0200

    Joke checking

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from 'react'
-import ContactCard from './components/ContactCard'
+import Joke from './components/Joke.js'
 
-function App(){
+export default function App(){
   return(
-      <div className="contacts">
-        <ContactCard contact={{
-          name: "Steve Jobs",
-          imgUrl: "https://fainaidea.com/wp-content/uploads/2019/01/apple8.0.jpg",
-          phone: "39493493",
-          email: "dfdfdf@mail.com"
-        }}/>
-        <ContactCard contact={{
-          name: "Nina Eva",
-          imgUrl: "https://andro-news.com/images/content/33691-59605-header-l.jpg",
-          phone: "37867866",
-          email: "dfdwewwee@mail.com"
-        }}/>
-        <ContactCard contact={{
-          name: "Vladimir Olegovich",
-          imgUrl: "https://www.apple.com/v/iphone/home/ac/images/meta/og__byyl1jl4xsvm.png",
-          phone: "7878756",
-          email: "cgvdf@mail.com"
-        }}/>
+    <div>
+      <Joke
+      question = "111"
+      answer = "222"
+      />
+      <Joke
+      question = "333"
+      answer = "444"
+      />
+      <Joke
+      question = "555"
+      answer = "666"
+      />
+      <Joke
+      answer = "666"
+      />
       </div>
     )
 }
-
-export default App
