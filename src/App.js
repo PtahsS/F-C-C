a864213a08a4069ca53fb850ecdd627c1e166d2d
PtahsS <ptahsrg@gmail.com>
2020-01-16 20:02:19 +0200

    UnreadMessages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-
 import React, {Component} from 'react'
-import Conditional from './components/Conditional'
 
 class App extends Component{
 	constructor(){
 		super()
-		this.state = {
-			isLoading:true
+		this.state={
+			unreadMessages: ["www", "ddd", "rrr"]
 		}
 	}
-	componentDidMount(){
-		setTimeout(()=>{
-			this.setState({
-				isLoading: false
-			})
-		}, 3000)
-	}
 	render(){
-
 		return(
 			<div>
-			<h1>Header</h1>
-			{(this.state.isLoading)? 
-				<h1>...isLoading</h1> : <Conditional isLoading={this.state.isLoading}/>
-			}
-			<h1>footer</h1>
+				{this.state.unreadMessages.length>0 &&
+					<h1>You have {this.state.unreadMessages.length} messages!</h1>
+				}
 			</div>
-			)
+		)
 	}
 }
 
